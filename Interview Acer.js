e1abc0c3bbea283baea60b6fd5c8f021fc035505
Mohammed Aamir Mansoori <aamir.mansoori2911@gmail.com>
2023-11-02 18:54:36 +0530

    Upload Form Completed!

--- Interview Acer.js
+++ Interview Acer.js
@@ -6,6 +6,7 @@ let btnList = document.getElementById("content-buttons");
 let uploadForm = document.getElementById("upload-details-form");
 let technicalRadio = document.getElementById("Technical");
 let nonTechnicalRadio = document.getElementById("Non-Technical");
+let technicalChoice = document.getElementById("technicalChoice");
 
 // Function to show a Element
 let showElement = (element) => {
@@ -27,26 +28,29 @@ btnUpload.addEventListener("click", () => {
 let formData = new FormData();
 
 //Setting Default Value to Form Data Object
-formData.set("interviewType", "Non-Technical");
+formData.set("Interview Type", "Non-Technical");
 
 btnUploadProceed.addEventListener("click", () => {
-        let resumeFile = document.getElementById("optionResume").files[0];
-        let jobDescription = document.getElementById("floatingTextarea2").value;
-        
-        formData.append("resume", resumeFile);
-        formData.append("jobDescription", jobDescription);
+    let resumeFile = document.getElementById("optionResume").files[0];
+    let jobDescription = document.getElementById("floatingTextarea2").value;
+    let inputTechnicalChoice = document.getElementById("inputTechnicalChoice").value;
 
-        // Use the formData object as needed (e.g., send it to a server, process it further, etc.)
-        let formDataObject = {};
 
-        for (let [key, value] of formData.entries()) {
-            formDataObject[key] = value;
-        }
+    formData.append("Resume", resumeFile);
+    formData.append("Job Description", jobDescription);
+    formData.append("Technical Choice", inputTechnicalChoice);
 
-        console.log(formDataObject);
-        // ... (additional code for handling the form data)
+    // Use the formData object as needed (e.g., send it to a server, process it further, etc.)
+    let formDataObject = {};
 
-        alert("Form submitted successfully!");
+    for (let [key, value] of formData.entries()) {
+        formDataObject[key] = value;
+    }
+
+    console.log(formDataObject);
+    // ... (additional code for handling the form data)
+
+    alert("Form submitted successfully!");
 });
 btnUploadCancel.addEventListener("click", () => {
     uploadForm.reset();
@@ -54,29 +58,32 @@ btnUploadCancel.addEventListener("click", () => {
     hideElement(uploadForm);
 });
 
-// Event listener for the resume input field
-let optionResume = document.getElementById("optionResume");
-optionResume.addEventListener("input", () => {
-    let fileErr = document.getElementById("fileErr");
-    fileErr.textContent = "";
-});
-
-// Event listener for the job description textarea
-let floatingTextarea2 = document.getElementById("floatingTextarea2");
-floatingTextarea2.addEventListener("input", () => {
-    let jobDescriptionErr = document.getElementById("jobDescriptionErr");
-    jobDescriptionErr.textContent = "";
-});
-
 // Event listener for Technical radio button
 technicalRadio.addEventListener("change", () => {
-    formData.set("interviewType", "Technical");
+    formData.set("Interview Type", "Technical");
+    technicalChoice.style.display = "flex";
 });
 
 // Event listener for Non-Technical radio button
 nonTechnicalRadio.addEventListener("change", () => {
-    formData.set("interviewType", "Non-Technical");
+    formData.set("Interview Type", "Non-Technical");
+    technicalChoice.style.display = "none";
 });
+
+// Event listener for the resume input field
+// let optionResume = document.getElementById("optionResume");
+// optionResume.addEventListener("input", () => {
+//     let fileErr = document.getElementById("fileErr");
+//     fileErr.textContent = "";
+// });
+
+// Event listener for the job description textarea
+// let floatingTextarea2 = document.getElementById("floatingTextarea2");
+// floatingTextarea2.addEventListener("input", () => {
+//     let jobDescriptionErr = document.getElementById("jobDescriptionErr");
+//     jobDescriptionErr.textContent = "";
+// });
+
 
 // Function to validate form fields
 // let validateForm = () => {
